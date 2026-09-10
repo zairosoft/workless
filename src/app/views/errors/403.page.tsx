509fc83953fb37fbea08fa0d979f5d6273f2411e
natt-natthawut <zairosoft@outlook.com>
2026-09-10 21:07:53 +0700

    [FIX] Error Page
    add
    <meta name="turbo-visit-control" content="reload" />
          <meta name="turbo-cache-control" content="no-cache" />

--- src/app/views/errors/403.page.tsx
+++ src/app/views/errors/403.page.tsx
@@ -1,5 +1,11 @@
 export const render403Page = createView<{}>(({ t }) => ({
   title: t('errors.403.pageTitle'),
+  head: (
+    <>
+      <meta name="turbo-visit-control" content="reload" />
+      <meta name="turbo-cache-control" content="no-cache" />
+    </>
+  ),
   bodyClassName: 'min-h-100vh flex grow bg-slate-50 text-slate-500 antialiased dark:bg-navy-900 dark:text-navy-200',
   children: (
     <main className="grid min-h-100vh w-full grow grid-cols-1 place-items-center">
